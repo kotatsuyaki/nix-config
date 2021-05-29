9b1dd0779a138b6707a4f1c8d0add5a8d1ecfd4f
Akitaki <robinhuang123@gmail.com>
2021-05-29 22:33:10 +0800

    Move mpd files to home

--- configuration.nix
+++ configuration.nix
@@ -74,17 +74,22 @@
     sound.enable = true;
 
     ### MPD ###
-    services.mpd.enable = true;
     hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
-    services.mpd.extraConfig = ''
-      follow_outside_symlinks "yes"
-      follow_inside_symlinks "yes"
-      audio_output {
-        type "pulse"
-        name "Pulseaudio"
-        server "127.0.0.1"
-      }
-    '';
+    services.mpd = {
+      enable = true;
+      user = "akitaki";
+      group = "users";
+      extraConfig = ''
+        music_directory "/home/akitaki/Music"
+        follow_outside_symlinks "yes"
+        follow_inside_symlinks "yes"
+        audio_output {
+            type "pulse"
+            name "Pulseaudio"
+            server "127.0.0.1"
+        }
+      '';
+    };
 
     ### User ###
     users.users.akitaki = {
