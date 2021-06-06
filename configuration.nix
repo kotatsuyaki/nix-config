927733985509cdffabf84e0f482968c358e7e0c8
Akitaki <robinhuang123@gmail.com>
2021-06-06 14:03:42 +0800

    Add teams to allowed unfree packages

--- configuration.nix
+++ configuration.nix
@@ -63,7 +63,7 @@ in {
     # Allow some nonfree packages
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "sublimetext3" "minecraft-launcher" "unrar" "nvidia-x11" "nvidia-settings" "nvidia-persistenced" "cudatoolkit"
-      "libtorch" "pytorch"
+      "libtorch" "pytorch" "teams"
     ];
 
     ### Basics ###
