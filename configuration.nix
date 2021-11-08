c71363cf514ee7eaec1a5b4ecf2d525b681f243c
Akitaki <robinhuang123@gmail.com>
2021-11-08 12:25:28 +0800

    Add haskell binary cache

--- configuration.nix
+++ configuration.nix
@@ -166,6 +166,14 @@ in
       automatic = true;
     };
 
+    ### Haskell binary cache
+    nix.binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    nix.binaryCaches = [
+      "https://hydra.iohk.io"
+    ];
+
     ### Virtualisation
     virtualisation = {
       libvirtd.enable = true;
