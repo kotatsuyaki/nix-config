ee7bdb5123294c82b974cc6a1bc511fbe1b6efe3
Akitaki <robinhuang123@gmail.com>
2021-12-02 06:57:22 +0000

    Format file

--- enable-flakes.nix
+++ enable-flakes.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }: {
   # Enable flakes
-  ({ pkgs, ... }: {
-    nix = {
-      package = pkgs.nixFlakes;
-      extraOptions = ''
-        experimental-features = nix-command flakes
-      '';
-    };
-  })
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 }
