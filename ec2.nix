d9cc7ddeb095a9cfc8534a1b19a621c86eda53aa
Akitaki <robinhuang123@gmail.com>
2021-12-02 06:57:09 +0000

    Remove import that caused infinite recursion

--- ec2.nix
+++ ec2.nix
@@ -1,9 +1,8 @@
-{ pkgs, inputs, ... }: {
-    imports = [ "${inputs.nixpkgs}/modules/virtualisation/amazon-image.nix" ];
-    ec2.hvm = true;
-    virtualisation.podman = {
-      enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-    };
+{ ... }: {
+  ec2.hvm = true;
+  virtualisation.podman = {
+  enable = true;
+    # Create a `docker` alias for podman, to use it as a drop-in replacement
+    dockerCompat = true;
+  };
 }
