a88e5378613012d18d734d0353dfd7976bb592e6
Akitaki <robinhuang123@gmail.com>
2021-12-05 12:52:49 +0800

    Format ec2.nix

--- ec2.nix
+++ ec2.nix
@@ -1,7 +1,7 @@
 { ... }: {
   ec2.hvm = true;
   virtualisation.podman = {
-  enable = true;
+    enable = true;
     # Create a `docker` alias for podman, to use it as a drop-in replacement
     dockerCompat = true;
   };
