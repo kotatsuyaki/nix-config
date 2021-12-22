71902e1184688af5b03b1dd1de5c917353150d64
Akitaki <robinhuang123@gmail.com>
2021-12-22 11:23:35 +0800

    System-wide encrypted DNS

--- network.nix
+++ network.nix
@@ -3,6 +3,25 @@
     useDHCP = false;
     networkmanager.enable = true;
     firewall.enable = false;
-    networkmanager.insertNameservers = [ "8.8.8.8" "8.8.4.4" ];
+
+    # Use DoH
+    nameservers = [ "127.0.0.1" "::1" ];
+    resolvconf.useLocalResolver = true;
+  };
+
+  # DoH service
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    upstreamDefaults = true;
+    settings = {
+      bootstrap_resolvers = [
+        "8.8.8.8:53" # google
+        "172.16.0.2:53" # library
+      ];
+      ipv6_servers = true;
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+    };
   };
 }
