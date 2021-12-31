96c6277a25333b05279d5ebfa29d90e9a07c1a2e
Akitaki <robinhuang123@gmail.com>
2021-12-31 13:53:16 +0800

    cgit/gitolite: Enable repo-specific hooks

--- cgit/default.nix
+++ cgit/default.nix
@@ -4,13 +4,16 @@ let
   # use customized highlighter script,
   # since the one shipped with cgit uses "pastie" theme,
   # which renders nix files with weird background colors
-  highlighter = pkgs.writers.writePython3Bin "highlighter" {
-    libraries = [ pkgs.python3Packages.pygments ];
-  } (builtins.readFile ./highlighter.py);
+  highlighter = pkgs.writers.writePython3Bin "highlighter"
+    {
+      libraries = [ pkgs.python3Packages.pygments ];
+    }
+    (builtins.readFile ./highlighter.py);
   head-include-file = pkgs.writeText
     "head-include"
     (builtins.readFile ./head-include.html);
-in {
+in
+{
   # cgit server
   services.lighttpd = {
     inherit port;
@@ -48,7 +51,7 @@ in {
   users.users.gitdaemon.extraGroups = [ "git" ];
   users.users.gitdaemon.isSystemUser = true;
   users.users.gitdaemon.group = "gitdaemon";
-  users.groups.gitdaemon = {};
+  users.groups.gitdaemon = { };
 
   # gitolite server
   services.gitolite = {
@@ -58,6 +61,9 @@ in {
     group = "git";
     extraGitoliteRc = ''
       $RC{UMASK} = 0027;
+      $RC{LOCAL_CODE} = "$rc{GL_ADMIN_BASE}/local";
+
+      push ( @{ $RC{ENABLE} }, 'repo-specific-hooks' );
     '';
     adminPubkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCiYMcZU/6ypxuUkfaTs4nX2s5rXTZ15VSbz/ftMvUPrkX0wOAwg5DXEguQgQ4xtf/qLzsjb0W48mPB18dpqGyFYsP6tdty6wKZLjp2+INOoySApHmE0QuL3UtzF53jizsinwbBKFwe/hmRBcWIBtYaj0TJt8/a3I00nQJ/RmQiw930CE7n33UqjCePDJfiZltdRIHOh0SB9B0gPHO2R3U6HbXT4bKKav8ZuKcTswuGwPNwr4VAJ3q12Kl0qFCHJQ5JuHUYTYAUGE/JHaAjtrPIktUK2ElmATxhfFNm+AEd7YDypZlYT6kb6OMisjjVYwrOg/pkFtIl+zw8SgA8zBY493rKiw87yA/v/so6+W84uoNAfJfcq+DnQliCNWxWJTgdptYrMUeNsswX4oMXgmTx4TQ/qCxmqY/bXLnGOT+mg98XQcDMlhwizzRtlJ5NH3A8BwHCArtDnjGaQb9PS5vJjk3wNFfw4Ngmxv6wDW0a88TYsJUC7Myirnw557Kl4is= akitaki@rx570-nixos";
   };
