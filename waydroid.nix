5b4a986d1137d33efb766face69643cb038fa503
Akitaki <robinhuang123@gmail.com>
2022-01-27 13:39:14 +0800

    waydroid.nix: Use mainline kernel 5.16

--- waydroid.nix
+++ waydroid.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   virtualisation.waydroid.enable = true;
   boot.kernelModules = [ "binder_linux" ];
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_5_16;
 }
