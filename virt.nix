e6a799d1a3217b9980b1fc15c1ad7027652dd7b9
Akitaki <robinhuang123@gmail.com>
2022-01-27 13:41:23 +0800

    virt.nix: Add quickemu and spice-gtk

--- virt.nix
+++ virt.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, system, ... }: {
   environment.systemPackages = with pkgs; [
     virt-manager
     podman
     appimage-run
+    inputs.unstable.legacyPackages.${system}.quickemu
+    spice-gtk
   ];
 
   virtualisation = {
+    spiceUSBRedirection.enable = true;
     libvirtd.enable = true;
     libvirtd.qemu.swtpm.enable = true;
     libvirtd.qemu.ovmf.package = (pkgs.OVMFFull.override {
