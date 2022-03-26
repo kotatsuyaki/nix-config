a90e6fb62c39b21eaf377a04970dfedbbab30694
Akitaki <robinhuang123@gmail.com>
2022-03-26 22:47:49 +0800

    services/mcserver.nix: Enable graceful shutdown
    * Remove dependency on nixFlakes and git by using the bot rewritten in
      Rust
    * Add socket to allow issuing /stop and other commands
    * Gracefully shutdown the server in ExecStop

--- services/mcserver.nix
+++ services/mcserver.nix
@@ -3,14 +3,24 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     description = "TerraFirmaCraft 1.7.10 server";
-    path = with pkgs; [
-      nixFlakes
-      git
-    ];
     serviceConfig = {
-      ExecStart = "/bin/sh -c 'nix develop --command java -Xmx6G -jar /home/akitaki/Documents/mcserver/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar -Dlog4j.configurationFile=log4j2_server.xml'";
+      ExecStart = "/bin/sh -c '. ./.env && ${pkgs.openjdk8_headless}/bin/java -Xmx6G -jar /home/akitaki/Documents/mcserver/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar -Dlog4j.configurationFile=log4j2_server.xml </run/minecraft.control'";
+      ExecStop = "/bin/sh -c 'echo stop > /run/minecraft.control'";
       WorkingDirectory = "/home/akitaki/Documents/mcserver";
+
       User = "akitaki";
+      Sockets = [ "mc-server.socket" ];
+    };
+  };
+  systemd.sockets.mc-server = {
+    bindsTo = [ "mc-server.service" ];
+    socketConfig = {
+      ListenFIFO = "/run/minecraft.control";
+      FileDescriptorName = "control";
+      RemoveOnStop = true;
+      SocketMode = "0660";
+      SocketUser = "akitaki";
+      SocketGroup = "users";
     };
   };
 }
