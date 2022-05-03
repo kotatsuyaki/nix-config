58f6e06edffaf191ac125362e91ed5855f5a2c1c
kotatsuyaki <robinhuang123@gmail.com>
2022-05-03 21:59:08 +0800

    fonts.nix: Add sans fonts

--- gui-modules/fonts.nix
+++ gui-modules/fonts.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }: {
   # Font packages
   fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
     source-han-serif
-    fira-code
-    fira-mono
-    inconsolata-nerdfont
+    source-han-sans
+
+    open-sans
+
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+    noto-fonts-extra
+
     iosevka-bin
     corefonts
   ];
