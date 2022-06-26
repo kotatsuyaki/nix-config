0b399c577c94eb24fa09a72b5d1984a0f882cafd
kotatsuyaki <robinhuang123@gmail.com>
2022-06-26 10:40:44 +0800

    nix-settings.nix: Add nix-community binary cache

--- default-modules/nix-settings.nix
+++ default-modules/nix-settings.nix
@@ -18,5 +18,14 @@
     binaryCachePublicKeys = [
       "kotatsuyaki.cachix.org-1:uX1H9hd4m+ZmqminrC2+oGp+1vIX95aHEuZ86Ja7/mE="
     ];
+
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 }
